4899daa342b555a2482d392eadc711d4c4cef1aa
Dmytro Nikulin <dimanikulin@gmail.com>
2026-01-22 08:50:14 +0200

    #2 moved created questionary for Features

---
+++ questionaryFeatures.js
@@ -0,0 +1,21 @@
+Ability to ask expert help in organizing album
+Online support
+Own cloud service to keep album
+Integration with a family tree tool 
+Ability to share album with family members
+Integration of AI/ML to provide a fully automatic flow
+Easy to use
+Fast to use
+Security for sensible content
+Identification the gaps inside of data
+Fixing/enriching the gaps in the data
+Integration with digiKam
+Integration with Synology
+Integration with other IR systems
+Search by events (text search)
+Search by photo author (text search)
+Search by places (text search)
+Search by people (text search)
+Linking recognized faces to people
+Reorganizing the existing album
+Recognition for emotions
